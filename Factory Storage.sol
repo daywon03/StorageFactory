a38b93b6913bd828db352789b4564f2be0eb0ea0
Damon <98743438+daywon03@users.noreply.github.com>
2024-03-27 11:52:53 +0100

    Add files via upload

---
+++ Factory Storage.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+
+contract FactoryStorage{
+    SimpleStorage[] listOfSimpleStorage;
+
+
+
+
+}
