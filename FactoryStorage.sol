29f83039f0769d6c86af47ff1356ba019773a26c
Damon Ba <damonba@MacBook-Air-de-Damon-2.local>
2024-03-28 22:20:06 +0100

    Finished to programmed the contract FactoryStorage

--- FactoryStorage.sol
+++ FactoryStorage.sol
@@ -8,6 +8,24 @@ contract FactoryStorage{
     SimpleStorage[] listOfSimpleStorage;
 
 
+    // This function create and add a new SimpleStorage to the listOfSimpleStorage 
+    function CreateSimpleStorage() public {
+        SimpleStorage _ststorage = new SimpleStorage();
+        listOfSimpleStorage.push(_ststorage);
+
+    }
+
+    //This function use the store function of SimpleStorage to attribute a favoNumber to the  ST that we createn and choose
+    function stStore(uint32 _stIndex, uint32 _stStorageNumber) public {
+        listOfSimpleStorage[_stIndex].store(_stStorageNumber);
+    }
+
+    function stGet(uint256 _stIndex) public view returns (uint256){
+        return listOfSimpleStorage[_stIndex].watch();
+    }
+
+    
+
 
 
 }
